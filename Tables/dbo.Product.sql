023caa981821ebe920ceddeb9d8e8c4122e21c70
RickCas <rmacaskill@me.com>
2015-04-13 08:11:12 +0100

    refactoring - removed SKU

--- Tables/dbo.Product.sql
+++ Tables/dbo.Product.sql
@@ -3,8 +3,7 @@ CREATE TABLE [dbo].[Product]
 [ID] [int] NOT NULL IDENTITY(1, 1),
 [LongName] [varchar] (400) COLLATE Latin1_General_CI_AS NULL,
 [ShortName] [varchar] (100) COLLATE Latin1_General_CI_AS NULL,
-[CategoryId] [int] NULL,
-[SKU] [varchar] (100) COLLATE Latin1_General_CI_AS NULL
+[CategoryId] [int] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Product] ADD CONSTRAINT [PK__Product__3214EC27567244CE] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
